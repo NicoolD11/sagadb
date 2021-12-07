db62b55fa65d4ff50a0946b4ff676a034966a43d
daborda4 <alejomestizo351@gmail.com>
2021-12-06 23:17:40 -0500

    cambio 5
    agrego navar

--- index.php
+++ index.php
@@ -8,10 +8,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>SAGADB</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">SAGA COMPLETA DRAGON BALL</span>
+  </div>
+</nav>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
