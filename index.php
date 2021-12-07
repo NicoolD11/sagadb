419374a054bb93964e697f9c6a9cab43a0d60d5e
daborda4 <alejomestizo351@gmail.com>
2021-12-07 10:28:12 -0500

    linkcss
    agregalinkcss

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
     <title>SAGADB</title>
   </head>
   <body>
     <!-- ENCABEZADO-->
-    <nav class="navbar navbar-light bg-light">
-    <div class="container-fluid">
+    <nav class="navbar navbar-light bg-light" >
+    <div class="container-fluid" class="conav">
         <span class="navbar-brand mb-0 h1" style="margin-left:45%;">SAGA COMPLETA DRAGON BALL</span>
     </div>
     </nav>
