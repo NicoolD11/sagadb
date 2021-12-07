1127c143208b42febb359cce980c9237ac84d840
daborda4 <alejomestizo351@gmail.com>
2021-12-07 10:57:35 -0500

    cambio17
    esta si fue

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     </nav>
 
 
-       <ul style="margin-left:20%;">
+       <ul style="margin-left:28%;">
             <li>DRAGON BALL</li>
             <div class="cajaimg">
         <a href="https://dragonball.sullca.com/capitulos"><img src="img/db.jpg" class="imagenes"></a>
